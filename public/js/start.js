ce981e83ec78f48d86a707a1ccc737554cd1b61f
anishashin <anishashin@gmail.com>
2022-02-15 14:20:26 -0500

    5 seconds disable button

--- public/js/start.js
+++ public/js/start.js
@@ -6,6 +6,9 @@
             contentType: 'application/json',
             beforeSend: function() {
                 $('#start').prop('disabled', true);
+                setTimeout(function() {
+                    $('#start').prop('disabled', false);
+                }, 5000);
             }
         };
         $.ajax(requestConfig).then(function(response) {
